a9844e0e80e79e1975a91582e38e7dcd6d88f4ad
LINDBURG <monc2170@gmail.com>
2022-04-03 20:14:46 +0900

    logic done

--- app/src/main/java/com/linbug/level/MainActivity.kt
+++ app/src/main/java/com/linbug/level/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.linbug.level
 
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.linbug.level.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private val sensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_GAME
+        )
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            binding.tiltView.onSensorEvent(event)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 }
---
+++ app/src/main/java/com/linbug/level/TiltView.kt
@@ -0,0 +1,72 @@
+package com.linbug.level
+
+import android.content.Context
+import android.graphics.Canvas
+import android.hardware.SensorEvent
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.linbug.level.databinding.ViewTiltBinding
+
+
+class TiltView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyle: Int = 0
+    ) : ConstraintLayout(context, attrs, defStyle) {
+    private var binding: ViewTiltBinding
+
+    private var cX: Float = 0f
+    private var cY: Float = 0f
+    private var hX: Float = 0f
+    private var hY: Float = 0f
+    private var vX: Float = 0f
+    private var vY: Float = 0f
+
+    private var centerAnim: Animation? = null
+    private var horizontalAnim: Animation? = null
+    private var verticalAnim: Animation? = null
+
+    private var oldX: Float = 0f
+    private var oldY: Float = 0f
+    private var xCoord: Float = 0f
+    private var yCoord: Float = 0f
+
+    init {
+        val layoutInflater = LayoutInflater.from(context)
+        binding = ViewTiltBinding.inflate(layoutInflater, this, true)
+
+    }
+
+
+    private fun updateView() {
+        centerAnim = TranslateAnimation(oldX * 12, xCoord * 12 , -oldY * 12, -yCoord * 12 )
+        centerAnim?.duration = 100
+        centerAnim?.fillAfter = true
+        binding.centerBubble.startAnimation(centerAnim)
+
+        horizontalAnim = TranslateAnimation(oldX * 30, xCoord * 30, 0f, 0f )
+        horizontalAnim?.duration = 100
+        horizontalAnim?.fillAfter = true
+        binding.horizontalBubble.startAnimation(horizontalAnim)
+
+        verticalAnim = TranslateAnimation(0f, 0f  , -oldY * 30, -yCoord * 30 )
+        verticalAnim?.duration = 100
+        verticalAnim?.fillAfter = true
+        binding.verticalBubble.startAnimation(verticalAnim)
+    }
+
+    fun onSensorEvent( event: SensorEvent) {
+        oldX = xCoord
+        oldY = yCoord
+
+        xCoord = event.values[0]
+        yCoord = event.values[1]
+        updateView()
+        invalidate()
+    }
+
+}
